b194f52f12a96b8391a19e1166d9558813a3f1ca
nilslambertz <nilslambertz@web.de>
2020-08-03 16:05:38 +0200

    added checkbox to turn autoclick on and off

--- popup.js
+++ popup.js
@@ -1,23 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     let button = document.getElementById("usernameChangeButton");
+    let checkbox = document.getElementById("autoclickInput");
     button.addEventListener('click', function() {
         let text = document.getElementById("usernameInput").value;
+        let checked = checkbox.checked;
         chrome.tabs.query({active: true, currentWindow: true}, tabs => {
             tabs.forEach(tab => {
-                chrome.tabs.sendMessage(tab.id, text);
+                chrome.tabs.sendMessage(tab.id, {username: text, autoclick: checked});
             });
         });
         chrome.storage.sync.set({"usernameData": text});
-        chrome.storage.sync.get(['usernameData'], function(r) {
-            console.log("XD1");
-            console.log(r);
-            console.log("XD2");
-        })
+        chrome.storage.sync.set({"autoclickData": checked});
+        window.close();
     })
 })
 
 chrome.storage.sync.get(['usernameData'], function(r) {
     if(r.usernameData !== null) {
         document.getElementById("usernameInput").value = r.usernameData;
+    }
+})
+
+
+chrome.storage.sync.get(['autoclickData'], function(r) {
+    if(r.autoclickData !== null) {
+        document.getElementById("autoclickInput").checked = r.autoclickData;
     }
 })
--- twitchScript.js
+++ twitchScript.js
@@ -1,40 +1,37 @@
 let username = "syrinxx1337";
+let autoclick = false;
 
 setInterval(function() {
-    updateUsername();
-    let arr = Array.from(document.getElementsByClassName("chat-line__message")).slice(-10);
-    for(let x of arr) {
-        if(x.classList.contains("read")) continue;
-        for(let outer of x.childNodes) {
-            if(outer.classList.contains("chat-line__username")) {
-                if(outer.innerText === username) {
-                    for(let c of x.childNodes) {
-                        if(c.classList.contains("link-fragment")) {
-                            let link = c.innerText;
-                            if(link.startsWith("https://daddyskins.com/arena")) {
-                                window.open(link);
+    if(autoclick) {
+        let arr = Array.from(document.getElementsByClassName("chat-line__message")).slice(-10);
+        for (let x of arr) {
+            if (x.classList.contains("read")) continue;
+            for (let outer of x.childNodes) {
+                if (outer.classList.contains("chat-line__username")) {
+                    if (outer.innerText === username) {
+                        for (let c of x.childNodes) {
+                            if (c.classList.contains("link-fragment")) {
+                                let link = c.innerText;
+                                if (link.startsWith("https://daddyskins.com/arena")) {
+                                    window.open(link);
+                                }
                             }
                         }
                     }
+                    break;
                 }
-                break;
             }
+            x.classList.add("read");
         }
-        x.classList.add("read");
     }
 },50);
 
-function updateUsername() {
-    try {
-        chrome.storage.sync.get(['usernameData'], function (r) {
-            if (r.usernameData === null || username === r.usernameData) return;
-            username = r.usernameData;
-        })
-    } catch(e) {
-        console.log("doof");
-    }
-}
-
 chrome.runtime.onMessage.addListener(msgObj => {
-    username = msgObj;
+    let newAutoclick = JSON.pars
+    if(msgObj.username !== null) {
+        username = msgObj.username;
+    }
+    if(msgObj.autoclick !== null) {
+        autoclick = msgObj.autoclick;
+    }
 });
