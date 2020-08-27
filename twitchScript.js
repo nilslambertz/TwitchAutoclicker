e7c2d8b69bb07aaf1c7e742f87f24eb48c8f569f
nilslambertz <nilslambertz@web.de>
2020-08-27 23:46:12 +0200

    removed default-values, fixed bugs where the extension wouldn't work in some chats by starting with inner node. added function updateValues() which gets called when receiving message

--- twitchScript.js
+++ twitchScript.js
@@ -1,40 +1,50 @@
-let username = "syrinxx1337";
+let username = "";
+let website = "";
 let autoclick = false;
+updateValues();
 
 setInterval(function() {
-    let arr = Array.from(document.getElementsByClassName("chat-line__message")).slice(-10);
-    for (let x of arr) {
+    let arr = Array.from(document.getElementsByClassName("chat-line__no-background")).slice(-10);
+
+    for(let x of arr) {
         if (x.classList.contains("read")) continue;
-        for (let outer of x.childNodes) {
-            if (outer.classList.contains("chat-line__username")) {
-                if (outer.innerText === username) {
-                    for (let c of x.childNodes) {
-                        if (c.classList.contains("link-fragment")) {
-                            let link = c.innerText;
-                            if (link.startsWith("https://daddyskins.com/arena") && autoclick) {
-                                window.open(link);
-                            }
-                        }
+        x.classList.add("read");
+
+        let name = "";
+
+        for(let c of x.childNodes) {
+            if(c.classList.contains("chat-line__username-container")) {
+                for(let child of c.childNodes) {
+                    if(child.classList.contains("chat-line__username")) {
+                        name = child.innerText;
                     }
                 }
-                break;
             }
         }
-        x.classList.add("read");
+
+        if(name.toLowerCase() === username.toLowerCase()) {
+            for(let child of x.childNodes) {
+                if(child.classList.contains("link-fragment")) {
+                    let link = child.innerText;
+                    if (link.startsWith("http") && link.includes(website) && autoclick) {
+                        window.open(link);
+                    }
+                }
+            }
+        }
     }
 },50);
 
-chrome.storage.sync.get(['usernameData', 'autoclickData'], function(items) {
-    username = items.usernameData;
-    autoclick = items.autoclickData;
-});
+function updateValues() {
+    chrome.storage.sync.get(['usernameData', 'autoclickData', 'websiteData'], function(items) {
+        username = items.usernameData ? items.usernameData : "";
+        autoclick = items.autoclickData;
+        website = items.websiteData ? items.websiteData : "";
+    });
+}
 
 chrome.runtime.onMessage.addListener(msgObj => {
-    let newAutoclick = JSON.pars
-    if(msgObj.username !== null) {
-        username = msgObj.username;
-    }
-    if(msgObj.autoclick !== null) {
-        autoclick = msgObj.autoclick;
+    if(msgObj.update) {
+        updateValues();
     }
 });
