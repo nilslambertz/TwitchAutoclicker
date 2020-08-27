5496a375da1a6ef8e0132b84f133da26ad765bb9
nilslambertz <nilslambertz@web.de>
2020-08-27 23:45:05 +0200

    popup sets all values now and sends update-request to content-script

--- popup.js
+++ popup.js
@@ -1,34 +1,46 @@
 let usernameData;
 let autoclickData;
+let websiteData;
 
 document.addEventListener('DOMContentLoaded', function() {
-    let button = document.getElementById("usernameChangeButton");
+    let button = document.getElementById("changeButton");
     let checkbox = document.getElementById("autoclickInput");
     button.addEventListener('click', function() {
-        let text = document.getElementById("usernameInput").value;
+        let username = document.getElementById("usernameInput").value.trim();
+        let website = document.getElementById("websiteInput").value.trim();
         let checked = checkbox.checked;
+
+        chrome.storage.sync.set({"usernameData": username});
+        chrome.storage.sync.set({"autoclickData": checked});
+        chrome.storage.sync.set({"websiteData": website});
+
         chrome.tabs.query({active: true, currentWindow: true}, tabs => {
             tabs.forEach(tab => {
-                chrome.tabs.sendMessage(tab.id, {username: text, autoclick: checked});
+                chrome.tabs.sendMessage(tab.id, {update:true});
             });
         });
-        chrome.storage.sync.set({"usernameData": text});
-        chrome.storage.sync.set({"autoclickData": checked});
+
         window.close();
     })
-})
+});
 
 chrome.storage.sync.get(['usernameData'], function(r) {
     if(r.usernameData !== undefined) {
         usernameData = r.usernameData;
         document.getElementById("usernameInput").value = usernameData
     }
-})
-
+});
 
 chrome.storage.sync.get(['autoclickData'], function(r) {
     if(r.autoclickData !== undefined) {
         autoclickData = r.autoclickData;
         document.getElementById("autoclickInput").checked = autoclickData;
     }
-})
+});
+
+chrome.storage.sync.get(['websiteData'], function(r) {
+    if(r.websiteData !== undefined) {
+        websiteData = r.websiteData;
+        document.getElementById("websiteInput").value = websiteData;
+    }
+});
